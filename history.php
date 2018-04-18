4f0ad695b5a9d8e2758ada52e99e756c4e4fbc07
Jessica Brown <jessica-e-brown@uiowa.edu>
2018-04-18 11:23:16 -0500

    timeline additions

--- history.php
+++ history.php
@@ -19,10 +19,10 @@
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
                 <li>
-                    <h4 class="history-table__update-heading">First Campus Program Grant</h3>
-                    <p>$199,668, established CCRT, Implemented mandatory education for incoming students, trained judicial administrators, trained law enforcement, established a men's anti-violence council.</p>
+                    <h4 class="history-table__update-heading">Received first Campus Program Grant from the U.S. Department of Justice, Office on Violence Against Women (OVW)</h4>
+                    <p>$199,668 to establish a Coordinated Campus Response Team (CCRT), implement mandatory education for incoming students, train judicial administrators, train law enforcement, and establish the <a href="https://wrac.uiowa.edu/get-involved/mens-anti-violence/">Men's Anti-Violence Council</a></p>
                 </li>
-                <li><h4 class="history-table__update-heading">CCRT Created - UI Anti-violence Coalition</h4></li>
+                <li><h4 class="history-table__update-heading">Created CCRT and <a href="https://osmrc.uiowa.edu/anti-violence-coalition">UI Anti-violence Coalition</a></h4></li>
             </ul>
         </div>
     </div>
@@ -33,10 +33,14 @@
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
                 <li>
-                    <h4 class="history-table__update-heading">First Sexual Miscodunct &amp; Dating/Domestic Violence Training</h3>
-                    <p>DVIP, RVAP, and DOS collaborate on sexual misconduct &amp; dating/domestic violence training</p>
+                    <h4 class="history-table__update-heading">Implemented first sexual misconduct and dating/domestic violence training for judicial administrators</h4>
+                    <p>Collaboration by <a href="https://dvipiowa.org/">Domestic Violence Intervention Program</a> (DVIP), <a href="https://rvap.uiowa.edu/">Rape Victim Advocacy Program</a> (RVAP), and Office of the Dean of Students</p>
                 </li>
-                <li><h4 class="history-table__update-heading">CCRT Created - UI Anti-violence Coalition</h4></li>
+                <li>
+                    <h4 class="history-table__update-heading">Received Iowa Regents Flagship Grant from the U.S. Department of Justice, Office on Violence Against Women</h4>
+                    <p>To work with the University of Northern Iowa (UNI) and Iowa State University (ISU) to establish a student Forum Theatre Troupe, enhance men's programming, provide specialized training for advocates in higher education, and develop a Gender Violence Curriculum Institute at UNI </p>
+                </li>
+                <li><h4 class="history-table__update-heading">Provided online sexual misconduct prevention education for all incoming undergraduates</h4></li>
             </ul>
         </div>    
     </div>
@@ -46,10 +50,7 @@
         </div>
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
-                <li>
-                    <h4 class="history-table__update-heading">First Sexual Miscodunct Policy</h4>
-                    <p>DVIP, RVAP, and DOS collaborate on sexual misconduct &amp; dating/domestic violence training</p>
-                </li>
+                <li><h4 class="history-table__update-heading">Created first sexual misconduct policy through the Iowa Regents Flagship Grant</h4></li>
             </ul>
         </div>    
     </div>
@@ -60,8 +61,10 @@
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
                 <li>
-                    <h4 class="history-table__update-heading">Created Office of the Sexual Misconduct Response Coordinator</h4>
+                    <h4 class="history-table__update-heading">Created <a href="https://osmrc.uiowa.edu/">Office of the Sexual Misconduct Response Coordinator</a> (OSMRC)</h4>
                 </li>
+                <li><h4 class="history-table__update-heading">Implemented new mandatory online sexual misconduct prevention education program for incoming undergraduate students</h4></li>
+                <li><h4 class="history-table__update-heading">President Mason required all faculty and staff appointed at 50% or greater to complete sexual harassment prevention training</h4></li>
             </ul>
         </div>    
     </div>
@@ -72,8 +75,66 @@
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
                 <li>
-                    <h4 class="history-table__update-heading">Created Office of the Sexual Misconduct Response Coordinator</h4>
+                    <h4 class="history-table__update-heading">Mandatory online sexual misconduct prevention education program required for incoming graduate/professional students</h4>
                 </li>
+                <li>
+                    <h4 class="history-table__update-heading">Received Iowa Regents Flagship Grant continuation funding</h4>
+                    <p>To engage in policy review and develop law enforcement online training</p>
+                </li>
+                <li><h4 class="history-table__update-heading"><a href="https://osmrc.uiowa.edu/campus-education-subcommittee-0">Campus Education Subcommittee</a> (CES) formed to create education to ensure students understood affirmative consent standard </h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2011</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li>
+                    <h4 class="history-table__update-heading">Received second Campus Program Grant from OVW</h4>
+                    <p>$300,000 to expand bystander intervention education, create the university’s stalking policy, and implement training to strengthen law enforcement and campus judicial investigations</p>
+                </li>
+                <li>
+                    <h4 class="history-table__update-heading">Strengthened victim advocacy resources</h4>
+                    <p>Engaged in a contract with DVIP to offer on-campus advocacy</p>
+                </li>
+                <li>
+                    <h4 class="history-table__update-heading">Bystander education included in On Iowa!</h4>
+                    <p>Orientation included a bystander workshop on dating violence</p>
+                </li>
+                <li><h4 class="history-table__update-heading">Collaborated with academic and professional departments to infuse the topic of gender-based violence and discrimination into courses</h4></li>
+                <li><h4 class="history-table__update-heading">CES shifted to align and coordinate prevention education efforts between providers</h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2012</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li>
+                    <h4 class="history-table__update-heading">Updated Johnson County Guidelines for Investigating Adult Sexual Assault</h4>
+                    <p>Included guidance and support for handling dual investigations (criminal and administrative)</p>
+                </li>
+                <li><h4 class="history-table__update-heading">Created and implemented campus judicial sexual assault investigator and adjudicator training</h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2013</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li><h4 class="history-table__update-heading">Published Campus Education Report</h4></li>
+                <li>
+                    <h4 class="history-table__update-heading">Created stalking policy language</h4>
+                    <p>Five policies modified to address stalking; National Stalking Resource Center provided guidance</p>
+                </li>
+                <li><h4 class="history-table__update-heading">Housing required all RAs & Pro Staff to receive bystander training</h4></li>
+                <li><h4 class="history-table__update-heading">Established monthly Clery case review with the Department of Public Safety, the Office of the Dean of Students, the Office of Equal Opportunity and Diversity, the Office of the General Counsel, UIHC Security Office, and the Office of the Sexual Misconduct Response Coordinator</h4></li>
             </ul>
         </div>    
     </div>
@@ -84,8 +145,50 @@
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
                 <li>
-                    <h4 class="history-table__update-heading">Created <a href="six-point-plan.php">the Six Point Plan</a></h4>
+                    <h4 class="history-table__update-heading">Received third Campus Program Grant from OVW</h4>
+                    <p>$300,000 to create the university’s dating/domestic policy, continue training to strengthen law enforcement and campus judicial investigations, develop a sanctioning intervention for non-expulsion cases, and continue on-campus advocacy services with DVIP</p>
                 </li>
+                <li>
+                    <h4 class="history-table__update-heading">Prevention education enhanced</h4>
+                    <p>Additional training required for orientation guides and Iowa Edge peer leaders; every new fraternity chapter member required to complete Every Member, Every Chapter training; and funding committed to 2.5 FTE prevention specialists</p>
+                </li>
+                <li>
+                    <h4 class="history-table__update-heading">UI Police Department received first department-wide training on sexual assault and stalking</h4>
+                    <p>8-hour training for all officers; judicial/policy investigators, municipal departments, and county attorney also participated</p>
+                </li>
+                <li><h4 class="history-table__update-heading">RVAP implemented <a href="https://endingviolence.uiowa.edu/e/community-collaboration/">Raise the Bar</a> bystander program for local bar staff</h4></li>
+                <li><h4 class="history-table__update-heading">Established <a href="https://dos.uiowa.edu/policies/sanctioning-guidelines-for-sexual-assault/">Sexual Assault Sanctioning Guidelines</a> for student misconduct</h4></li>
+                <li><h4 class="history-table__update-heading">RVAP created a campus advocate position</h4></li>
+                <li>
+                    <h4 class="history-table__update-heading">Created Title IX Team and established monthly meetings to review cases </h4>
+                    <p>Team included representation from the Office of the Sexual Misconduct Response Coordinator, the Office of Equal Opportunity and Diversity, the Office of the Dean of Students, the Office of the General Counsel, the Department of Public Safety, the Sexual Assault Nurse Examiner (SANE) program, and the Sexual Assault Response Team (SART)</p>
+                </li>
+                <li><h4 class="history-table__update-heading">Implemented <a href="https://osmrc.uiowa.edu/anti-violence-coalition/service-training">monthly training opportunities</a> for judicial administrators and AVC members</h4></li>
+                <li><h4 class="history-table__update-heading">President Mason introduced Six Point Plan to Combat Sexual Assault and established <a href="https://diversity.uiowa.edu/student-advisory-committee-sexual-misconduct">Student Advisory Committee on Sexual Misconduct</a></h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2015</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li>
+                    <h4 class="history-table__update-heading">UIPD received first department-wide training on dating/domestic violence</h4>
+                    <p>8-hour training for all officers, including a lunch panel of Title IX investigators and community partners</p>
+                </li>
+                <li>
+                    <h4 class="history-table__update-heading">Trained UI ITS security team member in phone and computer forensics to assist police and judicial investigations</h4>
+                    <p>Completed advanced digital forensics training from the SANS Institute</p>
+                </li>
+                <li>
+                    <h4 class="history-table__update-heading">Created dating/domestic violence policy language</h4>
+                    <p>Five policies modified to address dating/domestic violence; DVIP and RVAP participated in the policy writing process; <a href="http://muawi.org/">Monsoon Asians and Pacific Islanders in Solidarity</a> provided guidance</p>
+                </li>
+                <li><h4 class="history-table__update-heading">Commitment made to continue including bystander education in On Iowa! for all first-year students</h4></li>
+                <li><h4 class="history-table__update-heading">Administered first <a href="https://speakout.uiowa.edu/survey-results/survey-results-2015/">Speak Out Iowa campus climate survey</a> for all degree-seeking undergraduate, graduate, and professional students</h4></li>
+                <li><h4 class="history-table__update-heading">Established <a href="https://endingviolence.uiowa.edu/get-involved-2/certified-peer-prevention-education-trainer/">Certified Peer Educator Program</a> to expand bystander education and healthy relationships</h4></li>
             </ul>
         </div>    
     </div>
@@ -95,11 +198,27 @@
         </div>
         <div class="history-table__update-column">
             <ul class="history-table__update-list">
-                <li>
-                    <h4 class="history-table__update-heading"><a href="2016-plan.php">The University of Iowa Anti-Violence Plan
-for sexual misconduct, dating violence, and stalking
-Fall 2016 - Spring 2018</a></h4>
-                </li>
+                <li><h4 class="history-table__update-heading"><a href="https://speakout.uiowa.edu/survey-results/survey-results-2015/">Released results of Speak Out Iowa survey</a> and <a href="https://speakout.uiowa.edu/survey-results/survey-results-2015/ui-anti-violence-plan-2/">UI Anti-Violence Plan for Sexual Misconduct, Dating Violence, and Stalking (Fall 2016 – Spring 2018)</a></h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2017</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li><h4 class="history-table__update-heading">Administered second Speak Out Iowa campus climate survey for all degree-seeking undergraduate, graduate, and professional students</h4></li>
+            </ul>
+        </div>    
+    </div>
+    <div class="history-table">
+        <div class="history-table__year-column">
+            <h3 class="history-table__heading">2018</h3>
+        </div>
+        <div class="history-table__update-column">
+            <ul class="history-table__update-list">
+                <li><h4 class="history-table__update-heading">Released results of second Speak Out Iowa survey and UI Anti-Violence Plan for Sexual Misconduct, Dating Violence, and Stalking (Summer 2018 – Spring 2021)</h4></li>
             </ul>
         </div>    
     </div>
